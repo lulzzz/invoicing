302ef183e3d487af79daefb82f51c06929c09c4a
David <dafm1996@gmail.com>
2019-07-30 16:31:12 +0100

    added product and customer routers

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const connection = require('./db/mysql');
 const invoiceRouter = require('./routes/invoice');
 const companyRouter = require('./routes/company');
+const productRouter = require('./routes/product');
+const customerRouter = require('./routes/customer');
 
 const app = express()
 const port = process.env.PORT
@@ -9,6 +11,8 @@ const port = process.env.PORT
 app.use(express.json())
 app.use(invoiceRouter)
 app.use(companyRouter)
+app.use(productRouter)
+app.use(customerRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port', port);
