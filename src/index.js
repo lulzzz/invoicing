3cf73e54828b7e6e507e013da2b020b1e90298a2
David <dafm1996@gmail.com>
2019-07-30 11:40:49 +0100

    Added routes for company

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const connection = require('./db/mysql');
 const invoiceRouter = require('./routes/invoice');
+const companyRouter = require('./routes/company');
 
 const app = express()
 const port = process.env.PORT
 
 app.use(express.json())
 app.use(invoiceRouter)
+app.use(companyRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port', port);
