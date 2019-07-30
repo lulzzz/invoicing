fe729c56145286e100682276ad5f29222c2377c9
David <dafm1996@gmail.com>
2019-07-30 16:30:51 +0100

    added insert invoice with products and customers

--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -1,43 +1,63 @@
 const express = require('express');
 const router = new express.Router()
 const connection = require('../db/mysql');
-const {getCustomerId, getNoInvoices, insertInvoice} = require('../db/databaseHelper');
+const { getCustomerId, getNoInvoices, insertInvoice, assignProductsToInvoice, getProductId } = require('../db/databaseHelper');
 
 //Get all invoices
 router.get('/invoices', async (req, res) => {
 
     try {
-        var query = 'SELECT * FROM invoices '
-            + 'inner join invoices_products on invoices.idInvoice = invoices_products.idinvoice '
-            + 'inner join products on invoices_products.idProduct = products.idProduct '
+        var query = 'SELECT reference FROM invoices'
+        // + 'inner join invoices_products on invoices.idInvoice = invoices_products.idinvoice '
+        // + 'inner join products on invoices_products.idProduct = products.idProduct '
         connection.query(query, (error, results, fields) => {
             if (error) throw error
-
             res.send(results)
         })
-        // res.send(invoices)
     } catch (error) {
         res.status(400).send(error.message)
     }
-
 })
 
 //Create new invoice
 router.post('/invoices', async (req, res) => {
 
-    var invoiceType = req.body.type
-    var customerNIF = req.body.customer.nif
+    // {
+    //     "type": "FS/FR",
+    //     "customerNIF": "123456789",
+    //     "products": [{
+    //         "code": "xxxxx",
+    //         "unitPrice": 10,
+    //         "tax": 23,
+    //         "quantity": 2
+    //     ??? "discount": ???
+    //     }],
+    //     "date": "yyyy/mm/dd"
+    //     ?????? "vencimento": ?????
+    // }
 
-    var noInvoices = await getNoInvoices()
-    var customerId = await getCustomerId(customerNIF)
 
-    var reference = invoiceType + ' ' + new Date().getFullYear() + '/' + (noInvoices + 1)
 
-    var resp = await insertInvoice(reference, invoiceType, customerId)
-
-    res.send(resp)
+    try {
+        var invoiceType = req.body.type
+        var customerNIF = req.body.customerNIF
+        var products = req.body.products
+        var date = req.body.date
+    
+        var noInvoices = await getNoInvoices() 
+        //TODO arranjar um melhor sistema para numerar os invoices. o que acontece quando há um novo ano? invoices devem começar do 0 para cada ano
+        var customerId = await getCustomerId(customerNIF)
+    
+        var reference = invoiceType + ' ' + new Date(date).getFullYear() + '/' + (noInvoices + 1)
+    
+        var resp = await insertInvoice(reference, invoiceType, date, customerId)
+    
+        await assignProductsToInvoice(resp.idInvoice, products)
+    
+        res.send(resp.reference)
+    } catch (error) {
+        res.status(400).send(error)
+    }
 })
-
-
 
 module.exports = router
