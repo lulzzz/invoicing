9148beb68ecc34baeaa54bb57ae0fcd54477016d
David <dafm1996@gmail.com>
2019-07-30 16:29:30 +0100

    added todos. added helper functions

--- src/db/databaseHelper.js
+++ src/db/databaseHelper.js
@@ -1,7 +1,26 @@
 const connection = require('../db/mysql');
 
+var getProductId = (code) => {
+    return new Promise((resolve, reject) => {
+        let sql = "SELECT idProduct FROM products WHERE code = ?"
+        connection.query(sql, code,
+            function (err, result) {
+                if (err)
+                    reject(err);
+                else{
+                    console.log(result.length);
+                    if(result.length === 0){
+                        reject('Product with code ' + code + ' does not exist.')
+                        return
+                    }
+                    resolve(result[0].idProduct)
+                }
+            });
+    })
+}
+
 module.exports = {
-    getNoInvoices : () => {
+    getNoInvoices: () => {
         return new Promise((resolve, reject) => {
             connection.query('SELECT COUNT(*) AS invoiceCount FROM invoices', function (err, result) {
                 if (err)
@@ -13,28 +32,55 @@ module.exports = {
         })
     },
 
-    getCustomerId : (customerNIF) => {
+    getCustomerId: (customerNIF) => {
         return new Promise((resolve, reject) => {
             connection.query('SELECT idCustomer FROM customers where nif = ?', [customerNIF], function (err, result) {
                 if (err)
                     reject(err)
                 else
                     resolve(result[0].idCustomer);
+                    //TODO erro quando nao e encontrado nif
             });
         })
     },
 
-    insertInvoice : (reference, invoiceType, date, customerId) => {
+    insertInvoice: (reference, invoiceType, date, customerId) => {
         return new Promise((resolve, reject) => {
-            let sql = "INSERT INTO invoices (reference, type, createdAt, FK_idCustomer) VALUES ('" + reference + "', '" + invoiceType + "', '" + date + "', " + customerId + ")"
-            connection.query(sql, [reference],
+            values = [reference, invoiceType, date, customerId]
+            let sql = "INSERT INTO invoices (reference, type, createdAt, FK_idCustomer) VALUES (?)"
+            connection.query(sql, [values],
                 function (err, result) {
                     if (err)
-                        // res.status(400).send(err)
                         reject(err);
                     else
-                        resolve(reference)
+                        resolve({ "idInvoice": result.insertId, reference })
                 });
         })
+    },
+
+    assignProductsToInvoice: async (invoiceId, products) => {
+        var values = []
+        for await (const product of products) {
+            let productId = await getProductId(product.code)
+            product.id = productId
+            let tmp = []
+            tmp.push(invoiceId)
+            tmp.push(productId)
+            tmp.push(product.unitPrice)
+            tmp.push(product.quantity)
+            tmp.push(product.tax)
+            values.push(tmp)
+        }
+        console.log(values);
+        return new Promise((resolve, reject) => {
+            let sql = "INSERT INTO invoices_products (idInvoice, idProduct, unitPrice, quantity, tax) VALUES ?"
+            connection.query(sql, [values], function (err, result) {
+                if (err)
+                    reject(err);
+                else
+                    resolve(result)
+            });
+        });
+
     }
 }
