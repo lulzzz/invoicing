b199cac23354674b40e0aa9d2e33fddb43c98a77
David <dafm1996@gmail.com>
2019-07-30 11:40:22 +0100

    Added routes for company

--- src/routes/company.js
+++ src/routes/company.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const router = new express.Router()
+const connection = require('../db/mysql');
 
 //Create a new user
-router.post('/company', async (req, res) => {
-    var name = req.body.name
-    var nif = req.body.nif
-    var address = req.body.address
-    var postalCode = req.body.postalCode
-    var city = req.body.city
-    var country = req.body.country
+router.post('/company', (req, res) => {
+    var values = []
+    values.push(req.body.name)
+    values.push(req.body.nif)
+    values.push(req.body.address)
+    values.push(req.body.postalCode)
+    values.push(req.body.city)
+    values.push(req.body.country)
+    console.log(values);
+    var sql = "INSERT INTO company (`name`, `nif`, `address`, `postalCode`, `city`, `country`) VALUES (?)";
+    connection.query(sql, [values], function (err, result) {
+        if (err) res.status(400).send(err);
+        else res.send('OK')
+      });
+})
+
+router.get('/company', (req, res) => {
+    var sql = "SELECT * FROM company LIMIT 1";
+    connection.query(sql, function (err, result) {
+        if (err) res.status(400).send(err);
+        else res.send(result)
+      });
+})
+
+router.patch('/company', (req, res) => {
+    console.log(req.body);
+
+    var sql = "UPDATE company SET ?";
+    connection.query(sql, [req.body],  function (err, result) {
+        if (err) res.status(400).send('Bad request')
+        else res.send(result)
+      });
 })
 
 module.exports = router
