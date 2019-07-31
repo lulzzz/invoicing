79668be9eb8e4bc7c3f96270ffbfbacab6c99c73
David <dafm1996@gmail.com>
2019-07-31 12:02:01 +0100

    added validation to customer

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -1,10 +1,39 @@
 const express = require('express');
 const router = new express.Router()
 const connection = require('../db/mysql');
+const { check, validationResult } = require('express-validator');
 
 //Create a new customer
-router.post('/customers', (req, res) => {
-  //TODO return created customer?
+router.post('/customers', [ 
+  check('name')
+    .exists().withMessage('name field must exist')
+    .not().isEmpty().withMessage('name field must not be empty'),
+  check('nif')
+    .exists().withMessage('NIF must exist')
+    .not().isEmpty().withMessage('NIF must not be empty')
+    .isInt().withMessage('NIF must be a number')
+    .matches(/^[0-9]{9}/).withMessage('NIF must have 9 digits'),
+  check('address')
+    .exists().withMessage('address field must exist')
+    .not().isEmpty().withMessage('address field must not be empty'),
+  check('postalCode')
+    .exists().withMessage('postal code field must exist')
+    .not().isEmpty().withMessage('postal code field must not be empty')
+    .matches(/^([0-9]{4}-[0-9]{3})$/).withMessage('postal code should be in the form nnnn-nnn'),
+  check('city')
+    .exists().withMessage('city field must exist')
+    .not().isEmpty().withMessage('city field must not be empty'),
+  check('country')
+    .exists().withMessage('country field must exist')
+    .not().isEmpty().withMessage('country field must not be empty')
+], (req, res) => {
+
+  // send validation result
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).send(errors);
+  }
+
   var values = []
   values.push(req.body.name)
   values.push(req.body.nif)
@@ -12,50 +41,110 @@ router.post('/customers', (req, res) => {
   values.push(req.body.postalCode)
   values.push(req.body.city)
   values.push(req.body.country)
+
   var sql = "INSERT INTO customers (`name`, `nif`, `address`, `postalCode`, `city`, `country`) VALUES (?)";
   connection.query(sql, [values], function (err, result) {
-    if (err) res.status(400).send(err);
-    else res.status(201).send('OK')
+    if (err) {
+      switch (err.code) {
+        case 'ER_DUP_ENTRY':
+          res.status(409).send('Customer with NIF ' + req.body.nif + ' already exists')
+          break;
+        default:
+          res.status(400).send(err.sqlMessage)
+          break;
+      }
+    }
+    else res.status(201).send('Customer with NIF ' + nif + ' was created.')
   });
 })
 
 router.get('/customers', (req, res) => {
-  //TODO send only some params (dont send id)
-  var sql = "SELECT * FROM customers"; 
+  var sql = "SELECT name, nif, address, postalCode, city, country FROM customers";
   connection.query(sql, function (err, result) {
     if (err) res.status(400).send(err);
     else res.send(result)
   });
 })
 
-router.get('/customers/:nif', (req, res) => {
-  //TODO send only some params (dont send id)
-  //TODO send 404 if no customer is found
-  var sql = "SELECT * FROM customers where nif = ?";
-  connection.query(sql, [req.params.nif], function (err, result) {
-    if (err) res.status(400).send(err);
+router.get('/customers/:nif', [
+  check('nif')
+    .isInt().withMessage('NIF must be a number')
+    .matches(/^[0-9]{9}/).withMessage('NIF must have 9 digits')
+], (req, res) => {
+
+  // send validation result
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).send(errors);
+  }
+
+  var nif = req.params.nif
+  var sql = "SELECT name, nif, address, postalCode, city, country FROM customers WHERE nif = ?";
+  connection.query(sql, nif, function (err, result) {
+    if (err) {
+      res.status(400).send(err.sqlMessage);
+    }
+    else if (result.length === 0) {
+      res.status(404).send("Customer with NIF " + nif + " could not be found.")
+    }
     else res.send(result)
   });
 })
 
-router.patch('/customers/:nif', (req, res) => {
-  //TODO send 404 if no customer is found
-  //TODO set allowed params, all others should send forbidden (ids can be changed)
-  //TODO return updated customer?
+router.patch('/customers/:nif', [
+  check('nif')
+    .isInt().withMessage('NIF must be a number')
+    .matches(/^[0-9]{9}/).withMessage('NIF must have 9 digits')
+], (req, res) => {
+
+  // send validation result
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).send(errors);
+  }
+
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['name', 'nif', 'address', 'postalCode', 'city', 'country']
+  const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+  if (!isValidOperation) {
+    return res.status(400).send({ error: 'Invalid updates!' })
+  }
+
+  var nif = req.params.nif
   var sql = "UPDATE customers SET ? where nif = ?";
-  connection.query(sql, [req.body, req.params.nif], function (err, result) {
-    if (err) res.status(400).send(err)
-    else res.send(result)
+  connection.query(sql, [req.body, nif], function (err, result) {
+    if (err) {
+      res.status(400).send(err.sqlMessage);
+    }
+    else if (result.affectedRows === 0) {
+      res.status(404).send("Customer with NIF " + nif + " could not be found.")
+    }
+    else res.send('Customer with NIF ' + nif + ' was updated.')
   });
 })
 
-router.delete('/customers/:nif', (req, res) => {
-  //TODO send 404 if no customer is found
-  //TODO Return deleted customer?
+router.delete('/customers/:nif', [
+  check('nif')
+    .isInt().withMessage('NIF must be a number')
+    .matches(/^[0-9]{9}/).withMessage('NIF must have 9 digits')
+], (req, res) => {
+
+  // send validation result
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).send(errors);
+  }
+  var nif = req.params.nif
   var sql = "DELETE FROM customers where nif = ?";
-  connection.query(sql, [req.params.nif], function (err, result) {
-    if (err) res.status(400).send(err)
-    else res.send(result)
+  connection.query(sql, [nif], function (err, result) {
+    if (err) {
+      res.status(400).send(err.sqlMessage);
+    }
+    else if (result.affectedRows === 0) {
+      res.status(404).send("Customer with NIF " + nif + " could not be found.")
+    }
+    else res.send('Customer with NIF ' + nif + ' was deleted.')
   });
 })
 
