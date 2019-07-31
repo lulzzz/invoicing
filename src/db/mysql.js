9e5680ce2fbb0bb677feac01c4c5e83eb5ad205f
David <dafm1996@gmail.com>
2019-07-31 15:48:58 +0100

    added mysql pool

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,11 +1,12 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host     : process.env.MYSQL_HOST,
-    user     : process.env.MYSQL_USER,
-    password : process.env.MYSQL_PASSWORD,
-    database : process.env.MYSQL_DATABASE
+var pool = mysql.createPool({
+    connectionLimit: 10,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 });
-connection.connect();
 
-module.exports = connection
+
+module.exports = pool
