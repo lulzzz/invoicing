67b92814da3d0610446328b6a5765b3becf4a196
David <dafm1996@gmail.com>
2019-08-05 15:39:39 +0100

    added get invoice info, detailed info and get pdf

--- src/routes/invoice.js
+++ src/routes/invoice.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = new express.Router()
 const connection = require('../db/mysql');
 const validation = require('../middleware/validation');
-const { getCustomerId, getNoInvoices, insertInvoice, assignProductsToInvoice, getInvoiceDetails } = require('../db/databaseHelper');
+const { getCustomerId, getNoInvoices, insertInvoice, assignProductsToInvoice, getInvoiceInfo, getDetailedInvoiceInfo } = require('../db/databaseHelper');
+const generatePDF = require('../utils/generatePDF');
 
 //Get all invoices
 router.get('/invoices', async (req, res) => {
@@ -22,64 +23,42 @@ router.get('/invoices', async (req, res) => {
 
 //Get invoices by reference
 router.get('/invoices/:ref', async (req, res) => {
-    //TODO find how to manage reference with spaces in params
-    var ref = req.params.ref
-    var values = {}
 
-    var productsQuery = "SELECT products.code, products.description, invoices_products.unitPrice, invoices_products.quantity, invoices_products.tax "
-        + "FROM invoices inner join invoices_products "
-        + "on invoices.idInvoice = invoices_products.idinvoice "
-        + "inner join products on invoices_products.idProduct = products.idProduct "
-        + "WHERE invoices.reference = ?;"
-
-    var customerQuery = "SELECT customers.name, customers.nif, customers.address, customers.postalCode, customers.city "
-        + "FROM invoices inner join customers "
-        + "on invoices.FK_idCustomer = customers.idCustomer "
-        + "WHERE invoices.reference = ?;"
-
-    var companyQuery = "SELECT name, nif, address, postalCode, city, country from company"
-
-    var invoiceQuery = 'SELECT reference, createdAt FROM invoices where reference = ?'
-
-    connection.query(invoiceQuery, ref, function (err, result) {
-        if (err) return res.status(400).send(err);
-        else {
-            values.reference = result[0].reference
-            values.createdAt = result[0].createdAt
-            console.log(values);
-            connection.query(companyQuery, function (err, result) {
-                if (err) return res.status(400).send(err);
-                else {
-                    values.company = JSON.parse(JSON.stringify(result))
-                    connection.query(customerQuery, ref, function (err, result) {
-                        if (err) return res.status(400).send(err);
-                        else {
-                            values.customer = JSON.parse(JSON.stringify(result))
-                            connection.query(productsQuery, ref, function (err, result) {
-                                if (err) return res.status(400).send(err);
-                                else {
-                                    values.products = JSON.parse(JSON.stringify(result))
-                                    return res.send(values)
-                                }
-                            })
-                        }
-                    })
-                }
-            })
-        }
+    getInvoiceInfo(req.params.ref, async (err, values) => {
+        if (err) res.status(400).send(err)
+        else res.send(values)
     })
+
 })
 
-//Get invoices by reference
-router.get('/invoices/:ref/pdf', async (req, res) => {
-    getInvoiceDetails(req.params.ref, (values) => {
-        console.log(values);
-        //TODO
-        //get all elements of each tax and put in array
-        //multiply prices by tax
-        //pass everyting to PDF generator
+//Get invoice pdf by reference
+router.get('/invoices/:ref/pdf', (req, res) => {
+
+    getDetailedInvoiceInfo(req.params.ref, async (err, values) => {
+        if (err) res.status(400).send(err)
+        else {
+            try {
+                const pdf = await generatePDF(values, res)
+                res.contentType("application/pdf");
+                res.send(pdf);
+            } catch (error) {
+                res.status(400).send(error.message)
+            }
+
+        }
     })
-    
+
+})
+
+//Get detailed info from the invoice
+router.get('/invoices/:ref/info', (req, res) => {
+
+    getDetailedInvoiceInfo(req.params.ref, async (err, values) => {
+        if (err) res.status(400).send(err)
+        else {
+            res.send(values);
+        }
+    })
 })
 
 //Create new invoice
@@ -120,7 +99,7 @@ router.post('/invoices', validation.invoiceValidation, validation.validationResu
         /////Assign products to the created invoice
         var productToInvoice = await assignProductsToInvoice(resp.idInvoice, products)
 
-        //TODO gerar pdf da fatura? Guardar??
+        //TODO gerar pdf da fatura? Guardar pdf??
 
         res.status(201).send(resp.reference)
     } catch (error) {
