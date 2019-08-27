95b1a17789badca15529f4f5119e2a5f80b23dec
David <dafm1996@gmail.com>
2019-08-27 14:53:38 +0100

    added function to create db from sql file. currently commented

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var fs = require('fs');
 
 var pool = mysql.createPool({
     connectionLimit: 10,
@@ -6,7 +7,8 @@ var pool = mysql.createPool({
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE,
-    dateStrings: true
+    dateStrings: true,
+    multipleStatements: true
 });
 
 // this need to be here for select with large fields (lots of products for one invoice in saft)
@@ -15,5 +17,14 @@ pool.query('SET SESSION group_concat_max_len = 100000;', function (err, result) 
         console.log(err);
 })
 
+// To run the database creation script
+// var initQuery = fs.readFileSync(__dirname + '/db.sql').toString()
+
+// pool.query(initQuery, (err, result) => {
+//     if (err)
+//         console.log(err);
+//     else
+//         console.log(result);
+// })
 
 module.exports = pool
